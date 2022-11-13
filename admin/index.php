db0f979764779fffdad598bb6c6d48003b6b5748
Ctuanphuong18 <Ctuanphuong18@gmail.com>
2022-11-13 15:44:10 +0700

    update code

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,64 @@
-<?php 
-include 'controller/controller.php';
-?>
+<?php
+include 'view/header.php';
+
+//include dao để dùng các functione: 
+include "model/pdo.php";
+include "model/loai.php";
+// controller
+if (isset($_GET['act'])) {
+    $act = $_GET['act'];
+    switch ($act) {
+            // CONTROLLER LOẠI:
+        case "addl":
+            if(isset($_POST['btn_add']) && ($_POST['btn_add'])) { 
+                $name_cate = $_POST['name_cate'];
+                them_loai($name_cate);
+                $notice = "Thêm loại thành công !";
+            }
+            include "view/loai/add.php";
+            break;
+        case "listl":
+            $ds_loai = loadall_loai();
+            include "view/loai/list.php";
+            break;
+        case "sualoai": 
+            if(isset($_GET['id_cate']) && ($_GET['id_cate'] > 0)) { 
+                $id_cate = $_GET['id_cate'];
+                $one_loai = loadone_loai($id_cate);
+            }
+            include "view/loai/update.php";
+            break;
+            case "capnhatloai": 
+                if(isset($_POST['btn_update']) && ($_POST['btn_update'])) { 
+                    $id_cate = $_POST['id_cate'];
+                    $name_cate = $_POST['name_cate'];
+                    capnhat_loai($id_cate, $name_cate);
+                }
+                $ds_loai = loadall_loai();
+                include "view/loai/list.php";
+                break;
+                case "xoaloai": 
+                    if(isset($_GET['id_cate']) && ($_GET['id_cate'] > 0)) { 
+                    $id_cate = $_GET['id_cate'];
+                    xoa_loai($id_cate);
+                    }
+                    $ds_loai = loadall_loai();
+                    include "view/loai/list.php";
+                    break;
+            // CONTROLLER SẢN PHẨM:
+        case "addsp":
+            include "view/sanpham/add.php";
+            break;
+        case "listsp":
+            include "view/sanpham/list.php";
+            break;
+        default:
+            include "view/content.php";
+            break;
+    }
+} else {
+    include "view/content.php";
+}
+
+
+include 'view/footer.php';
--- admin/view/loai/list.php
+++ admin/view/loai/list.php
@@ -1,41 +1,48 @@
 <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <div class="card mb-4 mt-4">
-                            <div class="card-header">
-                                <i class="fas fa-table me-1"></i>
-                                Danh sách loại điện thoại 
-                            </div>
-                            <div class="card-body">
-                                <table id="datatablesSimple">
-                                    <thead>
-                                        <tr>
-                                            <th>Mã loại</th>
-                                            <th>Tên loại</th>
-                                            <th>Thao Tác</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                            <th>Mã loại</th>
-                                            <th>Tên loại</th>
-                                            <th>Thao tác</th>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
-                                        <tr>
-                                            
-                                            <td>1</td>
-                                            <td>iPhone</td>
-                                            <td class="text-center">
-                                                <a href="#" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i> Sửa</a>
-                                                <a href="" class="btn btn-danger"><i class="fa-solid fa-trash"></i> Xóa</a>
-                                            </td>
-                                        </tr>
-                                     
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </main>
+    <main>
+        <div class="container-fluid px-4">
+            <div class="card mb-4 mt-4">
+                <div class="card-header">
+                    <i class="fas fa-table me-1"></i>
+                    Danh sách loại điện thoại
+                </div>
+                <div class="card-body">
+                    <table id="datatablesSimple">
+                        <thead>
+                            <tr>
+                                <th>Mã loại</th>
+                                <th>Tên loại</th>
+                                <th>Thao Tác</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <th>Mã loại</th>
+                                <th>Tên loại</th>
+                                <th>Thao tác</th>
+                            </tr>
+                        </tfoot>
+                        <tbody>
+                            <?php
+                            foreach ($ds_loai as $loai) {
+                                extract($loai);
+                                $sualoai = "./index.php?act=sualoai&id_cate=" . $id_cate;
+                                $xoaloai = "./index.php?act=xoaloai&id_cate=" . $id_cate;
+                                echo ' <tr>        
+                                <td>'.$id_cate.'</td>
+                                <td>'.$name_cate.'</td>
+                                <td class="text-center">
+                                    <a href="'.$sualoai.'" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i> Sửa</a>
+                                    <a href="'.$xoaloai.'" class="btn btn-danger"><i class="fa-solid fa-trash"></i> Xóa</a>
+                                </td>
+                            </tr>
+                         ';
+                            } 
+                            ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
