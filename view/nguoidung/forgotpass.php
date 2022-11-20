04df05f132c5537429770d38e5b57a49017c4388
minhnv20397 <zatuzick03@gmail.com>
2022-11-20 13:39:51 +0700

    Quên mật khẩu

--- view/nguoidung/forgotpass.php
+++ view/nguoidung/forgotpass.php
@@ -16,20 +16,34 @@
         <div class="row" style="justify-content: center;">
             <!-- form đăng ký -->
             <div class="col-sm-12 col-md-12 col-lg-6 col-xs-12">
-                <form action="index.php?act=register" method="post">
+                <form action="index.php?act=forgotPass" method="post">
                     <div class="login-form">
+                        <?php  
+                        if(isset($_POST['btn_forgotPass'])){
+                            $error = array();
+                            $email= $_POST['email'];
+                                if($email == ""){
+                                    $error['email']='Không để trống Email!';
+                                }
+                                if(empty($error)){
+                                    $result =getUserEmail($email);
+                                    $code = substr(rand(0,999999),0,6);
+                                    $title ="Quên mật khẩu";
+                                    $content = "Mã xác thực của bạn là: <span style='color: red'>".$code."</span>";
+                                    $mail->sendMail($title, $content, $email);
+                                    $_SESSION['mail'] = $email;
+                                    $_SESSION['code'] = $code;
+                                }}
+                                ?>
                         <h4 class="login-title">Tìm lại mật khẩu</h4>
                         <div class="row">
                             <div class="col-md-12 col-12">
-                                <labe>Tên tài khoản</labe>
-                                <input type="email" placeholder="Nhập tên tài khoản của bạn">
-                            </div>
-                            <div class="col-md-12 col-12">
                                 <labe>Email</labe>
-                                <input type="email" placeholder="Nhập địa chỉ email đăng ký">
+                                <input type="email" style="margin-top: 10px;" name="email" placeholder="Nhập địa chỉ email đăng ký">
+                                <span style="color: red; "><?php if(isset($error['email'])) echo $error['email']  ?></span> <br>
                             </div>
                             <div class="col-12 wrap-btn-sub">
-                                <input type="submit" class="btn-submit" name="btn_findpass" value="Tìm mật khẩu">
+                                <input type="submit" class="btn-submit" name="btn_forgotPass" value="Gửi yêu cầu">
                             </div>
                         </div>
                     </div>
