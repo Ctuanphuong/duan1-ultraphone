a787282fd05edc921e20c38078d3352b34a69e8a
Ctuanphuong18 <Ctuanphuong18@gmail.com>
2022-12-02 15:45:52 +0700

    update

--- admin1/view/dashboard.php
+++ admin1/view/dashboard.php
@@ -20,26 +20,15 @@
 
             <!-- Content Row -->
             <div class="row">
-<<<<<<< HEAD
 
                <!-- Earnings (Monthly) Card Example -->
                <div class="col-xl-3 col-md-6 mb-4">
-=======
-                <?php $abc = countusser(); ?>
-                <!-- Earnings (Monthly) Card Example -->
-                <div class="col-xl-3 col-md-6 mb-4">
->>>>>>> d6430967ee17ab949e9be94a5f82acd439895823
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
-<<<<<<< HEAD
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Tổng doanh thu(Tuần)</div>
                                     <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
-=======
-                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Tổng thành viên</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800"><?php $abc ?></div>
->>>>>>> d6430967ee17ab949e9be94a5f82acd439895823
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
