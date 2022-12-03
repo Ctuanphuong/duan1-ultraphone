04f4ad86d678b5c5ca3f3955fb9db0bbbceb70d4
Your Name <you@example.com>
2022-12-03 17:35:27 +0700

    update thống kê danh mục

--- admin/model/thongke.php
+++ admin/model/thongke.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 function loadall_statis()
 {
     $sql = "SELECT c.id_cate as idcate, c.name_cate as namecate, count(p.id_pro) as pro_quantity, min(p.price) as min_price, max(p.price) as max_price, avg(p.price) as avg_price";
@@ -6,6 +6,13 @@ function loadall_statis()
     $sql .= " GROUP BY c.id_cate ORDER BY c.id_cate DESC";
     $liststatis = pdo_query($sql);
     return $liststatis;
+}
+
+function thonngke()
+{
+    $sql = "SELECT *,COUNT(product.name_pro) AS sluong FROM product INNER JOIN category ON product.idcate = category.id_cate GROUP BY category.name_cate;";
+    $a = pdo_query($sql);
+    return $a;
 }
 
 function ngay()
